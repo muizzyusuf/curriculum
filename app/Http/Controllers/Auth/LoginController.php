8054dbb8073f2b4053fca88295e367d21f77c3cc
muizzyusuf <abdelmuizz.yusuf@ubc.ca>
2020-10-02 16:36:12 -0700

    commited

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
     /**
      * Where to redirect users after login.
      *
-     * @var string
+     * @var string 
      */
 
     //protected $redirectTo = RouteServiceProvider::HOME;
